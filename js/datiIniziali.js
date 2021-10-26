63bd8bf6ce91ce0e87650d361bf78683bb412ab2
flashbLack98 <npalakuttige98@icloud.com>
2021-10-26 17:16:18 +0200

    start milestone 3

--- js/datiIniziali.js
+++ js/datiIniziali.js
@@ -1,4 +1,4 @@
-const icon = [
+const iconList = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -96,3 +96,82 @@ const icon = [
 		family: 'fas'
 	}
 ];
+
+const selectType = document.getElementById("select");
+
+const container = document.querySelector(".container");
+console.log("questo è il container", container);
+
+
+let color = "";
+
+
+
+
+for (i = 0; i < iconList.length; i++) {
+	const singleIcon = iconList[i];
+
+
+	const typeAnimal = iconList.filter((singleIcon) => {
+		return singleIcon.type === "Animal";
+	});
+	console.log(typeAnimal);
+
+	const { name, prefix, type, family } = singleIcon;
+
+	//COLORO LE ICONE IN BASE AL TYPE CHE HANNO
+	//se il type è animal allora il colore sarà blu
+	if (type === "user") {
+		color = "text-primary";
+	} //se il type è user allora il colore sarà viola
+	else if (type === "animal") {
+		color = "text-success";
+	}  //se il type è verdure allora il colore sarà arancione 
+	else {
+		color = "text-warning";
+	}
+	console.log(type);
+
+	const card = `<div class="card">
+                <i class="${family} ${prefix}${name} fs-3 ${color}"></i>
+                <p class="m-0">${name}</p>
+            </div>`;
+
+	console.log("questa è una carta", card, "questa è una icona", singleIcon.type);
+
+	container.innerHTML += card;
+
+
+
+}
+
+const formType = selectType.value;
+console.log(formType);
+
+
+
+
+
+//quando nel select c'è Animal devo stampare solo le card con type animal
+
+//quando nel select c'è Vegetable devo stampare solo le card con type Vegetable
+
+//quando nel select c'è User devo stampare solo le card con type User
+
+//quando nel select c'è All devo stampare tutte le card 
+
+
+
+
+
+
+
+
+
+
+/* Milestone 1
+Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+Milestone 2
+Coloriamo le icone per tipo
+Milestone 3
+Creiamo una select con i tipi di icone e usiamola per filtrare le icone */
